785f4886ca5b95f0debd51db4960cbadd7abac18
marovichn <nikolamarovic.11@gmail.com>
2023-04-26 23:19:21 +0200

    deployement

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ app.post("/failure", function (req, res) {
   res.redirect("/");
 });
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log(`Server running locally on port [ ${process.env.PORT} ]`);
 });
